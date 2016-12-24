9c0833cc922b04575c61ddcd817c7b8dc6df7c22
Tomy8s <tomy8s@hotmail.com>
2016-12-24 00:19:25 +0000

    test Jumbler raises errors

--- ruby/spec/jumbler_spec.rb
+++ ruby/spec/jumbler_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe Jumbler do
   describe '.jumble' do
-    it 'returns an array with 100 items' do
-      expect(subject.class.jumble.length).to eq 100
+    context 'no params' do
+      it 'returns an array with 100 items' do
+        expect(subject.class.jumble.length).to eq 100
+      end
     end
 
-    context 'given params' do
+    context 'given correct params' do
       leng = rand(100)
       max = rand(500)
       min = rand(-100..50)
@@ -26,6 +28,28 @@ describe Jumbler do
         jumb = subject.class.jumble(100, min)
         jumb.each do |i|
           expect(i).to be >= min
+        end
+      end
+
+      context 'given erroneous params' do
+        it 'throws an error for length less than 1' do
+          expect{ subject.class.jumble(-1) }.to raise_error ArgumentError
+        end
+
+        it 'throws an error for length not Fixnum' do
+          expect{ subject.class.jumble('hello') }.to raise_error ArgumentError
+        end
+
+        it 'throws an error for min not Fixnum' do
+          expect{ subject.class.jumble(100, 'hello') }.to raise_error ArgumentError
+        end
+
+        it 'throws an error for max not Fixnum' do
+          expect{ subject.class.jumble(100, -100, 'hello') }.to raise_error ArgumentError
+        end
+
+        it 'throws an error if min is more than max' do
+          expect{ subject.class.jumble(100, 100, -100) }.to raise_error ArgumentError
         end
       end
     end
